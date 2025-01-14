3f17e3d135a897fc697ac664f42d320c56f21050
Ekow <ftn2mr@virginia.edu>
2025-01-14 10:56:38 -0500

    made clas adhere to single resonsibility principle

--- telemetry.py
+++ telemetry.py
@@ -1,57 +1,47 @@
 import socket 
-import os
-from salsa20 import Salsa20_xor 
-import struct
 from lap import Lap
 from gt_data import GT_Data
-
-
-def salsa20_dec(dat):
-    key = b'Simulator Interface Packet GT7 ver 0.0'
-    # Seed IV is always located here
-    oiv = dat[0x40:0x44]
-    iv1 = int.from_bytes(oiv, byteorder='little')
-    # Notice DEADBEAF, not DEADBEEF
-    iv2 = iv1 ^ 0xDEADBEAF
-    iv = bytearray()
-    iv.extend(iv2.to_bytes(4, 'little'))
-    iv.extend(iv1.to_bytes(4, 'little'))
-    ddata = Salsa20_xor(dat, bytes(iv), key[0:32])
-    magic = int.from_bytes(ddata[0:4], byteorder='little')
-    if magic != 0x47375330:
-        return bytearray(b'')
-    return ddata
+from decoder import salsa20_dec
+from queue import Queue
 
 #TODO: Add debugging for socket connection 
 class Telemetry():
-    def __init__(self):
+    def __init__(self,data_queue):
         self.playstation_ip =  "10.41.1.59"
         self.receive_port = 33740
         self.send_port  = 33739
+        self.last_time_data_received = 0
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.data_queue = data_queue
         self.set_configs()
-    
-    
+        
     def send_hb(self):
-        "Sends a heartbeat to the playstation to ensure both ends are aware of the connection "
         send_data = 'A'
         self.socket.sendto(send_data.encode('utf-8'), (self.playstation_ip, self.send_port))
     
     def bind_socket(self):
         self.socket.bind(("0.0.0.0",self.receive_port))
+
         
     def set_timeout(self, timeout):
         self.socket.settimeout(timeout)
     
     def set_configs(self):
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.bind_socket()
         self.set_timeout(10)
     
     def get_data(self):
         iterations = 0
+      
+        self.send_hb()
         while True:
             iterations+=1
-               
+            
+            if iterations > 100:
+                self.send_hb()
+                iterations = 0
+                
             data,addr = self.socket.recvfrom(4096)
             decoded_data = salsa20_dec(data)
             gt_data = GT_Data(decoded_data)
@@ -60,11 +50,11 @@ class Telemetry():
             gt_data.last_lap_time = Lap(gt_data.current_lap_number, gt_data.last_lap_time).lap_time if gt_data.last_lap_time != -1 else "0:00.000"
             
             print(gt_data.to_dict())
+           
+            self.data_queue.put((gt_data.time_on_track, gt_data.throttle_percent))
             
-            if iterations > 100:
-                self.send_hb()
-                iterations = 0
 
 if __name__ == "__main__":
-    telemetry = Telemetry()
+    telemetry = Telemetry(Queue())
     telemetry.get_data()
+    
